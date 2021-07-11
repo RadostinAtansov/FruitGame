f926593ead6e3511187af263d7c11fdc5d9462b2
Radostin Atanasov <radstatan@gmail.com>
2021-07-11 21:53:10 +0300

    commit

---
+++ FruitGame/FruitGame/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using FruitGame.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FruitGame.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        static int rowsCount = 3;
+        static int colsCount = 9;
+        static string[,] fruits = GenerateRandomFruits();
+        static int score = 0;
+        static bool gameOver = false;
+
+        public IActionResult Index()
+        {
+            ViewBag.rowsCount = rowsCount;
+            ViewBag.colsCount = colsCount;
+            ViewBag.fruits = fruits;
+            ViewBag.score = score;
+            ViewBag.gameOver = gameOver;
+            return View();
+        }
+
+        public IActionResult Reset()
+        {
+            fruits = GenerateRandomFruits();
+            gameOver = false;
+            score = 0;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult FireTop(int position)
+        {
+            return Fire(position, 0, 1);
+        }
+
+        public IActionResult FireBottom(int position)
+        {
+            return Fire(position, rowsCount - 1, -1);
+        }
+
+        private IActionResult Fire(int position, int startRow, int step)
+        {
+
+            var col = position * (colsCount - 1) / 100;
+            var row = startRow;
+            while (row >= 0 && row < rowsCount)
+            {
+                var fruit = fruits[row, col];
+                if (fruit == "apple" || fruit == "banana" || fruit == "orange" || fruit == "kiwi")
+                {
+                    score++;
+                    fruits[row, col] = "Empty";
+                    break;
+                }
+                else if (fruit == "dynamite") { gameOver = true; break; }
+                
+            }
+            return RedirectToAction("Index");
+        }
+
+        static string[,] GenerateRandomFruits()
+        {
+            var rand = new Random();
+            fruits = new string[rowsCount, colsCount];
+            for (int row = 0; row < rowsCount; row++)
+            {
+                for (int col = 0; col < colsCount; col++)
+                {
+                    var r = rand.Next(9);
+                    if (r < 2) fruits[row, col] = "apple";
+                    else if (r < 4) fruits[row, col] = "banana";
+                    else if (r < 6) fruits[row, col] = "orange";
+                    else if (r < 8) fruits[row, col] = "kiwi";
+                    else fruits[row, col] = "dynamite";
+                }
+            }
+            return fruits;
+        }
+
+    }
+
+}
